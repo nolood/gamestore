2cb526b14ec7808fa3c478577b60c9b16b012994
nolood <a9514548799@gmail.com>
2023-04-23 14:07:16 +0500

    -add

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,6 +1,6 @@
 export const API_KEY = '95bf0e511dcf4f83937536114e0b0d8b'
 
-export const HOME_ROUTE = '/'
-export const GAME_ROUTE = '/game'
-export const CATALOG_ROUTE = '/catalog'
+export const HOME_ROUTE = '/gamestore'
+export const GAME_ROUTE = '/gamestore/game'
+export const CATALOG_ROUTE = '/gamestore/catalog'
 
